c2b95f782eeba64fdf615353d9445629990b6566
ansiam <166173295+ansiam@users.noreply.github.com>
2026-04-03 02:36:34 +0600

    Update ViolationLog.cs

--- ObserveX.Api/Models/ViolationLog.cs
+++ ObserveX.Api/Models/ViolationLog.cs
@@ -8,25 +8,13 @@ public class ViolationLog
 {
     public int Id { get; set; }
 
-    
-    public string Message { get; set; } = string.Empty;
-
-    public DateTime Timestamp { get; set; } = DateTime.Now;
-
-     public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public string Address { get; set; } = string.Empty;
-    public string Country { get; set; } = string.Empty;
-    public string City { get; set; } = string.Empty;
-    public string ZipCode { get; set; } = string.Empty;
-}
-
-
-public int Id { get; set; }
-
     public string StudentEmail { get; set; } = string.Empty;
 
     public string TeacherEmail { get; set; } = string.Empty;
 
     public string CourseName { get; set; } = string.Empty;
+
+    public string Message { get; set; } = string.Empty;
+
+    public DateTime Timestamp { get; set; } = DateTime.Now;
 }
